14c6302bbb0243220ca7b367beb7c59d013de185
jakemunroe <jmunroe99@gmail.com>
2024-04-10 15:05:31 -0500

    Completed Workshop 9

--- workshop9-forensics/workshop9.py
+++ workshop9-forensics/workshop9.py
@@ -12,10 +12,16 @@ def readData():
     logObj  = myLogger.giveMeLoggingObject()
     iris = datasets.load_iris()
     print(type(iris.data), type(iris.target))
+
+    # We want to check what kind of data is being loaded in to make sure it is
+    # not erronous data that is the result of a poisoning attack.
+    logObj.info('Data types read by file:\n\t %s\n', type(iris.data))
+    
     X = iris.data
     Y = iris.target
     df = pd.DataFrame(X, columns=iris.feature_names)
     print(df.head())
+    
     # Since poisoning attacks focus on injecting erronous data into the dataset,
     # I am going to log the data that is being read by the ML code.
     logObj.info('Dataset file to be read:\n %s\n', df.head())
@@ -24,7 +30,9 @@ def readData():
 
 def makePrediction():
     logObj  = myLogger.giveMeLoggingObject()
+    
     iris = datasets.load_iris()
+
     knn = KNeighborsClassifier(n_neighbors=6)
     knn.fit(iris['data'], iris['target'])
     X = [
@@ -33,6 +41,7 @@ def makePrediction():
     ]
     prediction = knn.predict(X)
     print(prediction)
+
     # Since model tricking occurs when models are attacked and cause them to make
     # erronous results/affect prediction performace, I am going to log the
     # predictions that are made by the ML code.
@@ -40,6 +49,13 @@ def makePrediction():
 
 def doRegression():
     diabetes = datasets.load_diabetes()
+    
+    logObj  = myLogger.giveMeLoggingObject()
+    
+    # We want to check what kind of data is being loaded in to make sure it is
+    # not erronous data that is the result of a poisoning attack.
+    logObj.info("Dataset loaded into Regression Function: \n\t %s\n", diabetes)
+
     diabetes_X = diabetes.data[:, np.newaxis, 2]
     diabetes_X_train = diabetes_X[:-20]
     diabetes_X_test = diabetes_X[-20:]
@@ -49,8 +65,15 @@ def doRegression():
     regr.fit(diabetes_X_train, diabetes_y_train)
     diabetes_y_pred = regr.predict(diabetes_X_test)
 
+    # Since model tricking occurs when models are attacked and cause them to make
+    # erronous results/affect prediction performace, I am going to log the
+    # predictions that are made by the ML code.
+    logObj.info('Prediction made by algorithm: %s\n', diabetes_y_pred)
+
 
 def doDeepLearning():
+    logObj  = myLogger.giveMeLoggingObject()
+    
     train_images = mnist.train_images()
     train_labels = mnist.train_labels()
     test_images = mnist.test_images()
@@ -90,11 +113,20 @@ def doDeepLearning():
     validation_data=(test_images, to_categorical(test_labels)),
     )
 
+    # Since poisoning attacks focus on injecting erronous data into the dataset,
+    # I am going to log the data that is being read by the ML code.
+    logObj.info('Validation data for deep learning:\n\t %s\n', validation_data)
+
     model.save_weights('cnn.h5')
 
     predictions = model.predict(test_images[:5])
 
     print(np.argmax(predictions, axis=1)) # [7, 2, 1, 0, 4]
+
+    # Since model tricking occurs when models are attacked and cause them to make
+    # erronous results/affect prediction performace, I am going to log the
+    # predictions that are made by the ML code.
+    logObj.info('Prediction made by deep learning model: \t%s\n', predictions)
 
     print(test_labels[:5]) # [7, 2, 1, 0, 4]
 
